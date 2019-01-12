e419f49e13c2e45b304aa44ec24ac2176877f81e
lishanquan <991887313@qq.com>
2019-01-12 22:09:42 +0800

    修改LogoutRequestPacket

--- src/main/java/the/daniel/server/handler/LogoutRequestHandler.java
+++ src/main/java/the/daniel/server/handler/LogoutRequestHandler.java
@@ -2,6 +2,7 @@ package the.daniel.server.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import the.daniel.protocol.request.LogoutRequestPacket;
 import the.daniel.protocol.response.LogoutResponsePacket;
 import the.daniel.util.SessionUtil;
 
@@ -9,10 +10,10 @@ import the.daniel.util.SessionUtil;
  * @Author: Daniel
  * @Date: 2019/1/11 14:53
  */
-public class LogoutRequestHandler extends SimpleChannelInboundHandler<LogoutRequestHandler> {
+public class LogoutRequestHandler extends SimpleChannelInboundHandler<LogoutRequestPacket> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, LogoutRequestHandler logoutRequestHandler) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, LogoutRequestPacket logoutRequestPacket) throws Exception {
         SessionUtil.unBindSession(ctx.channel());
         LogoutResponsePacket logoutResponsePacket = new LogoutResponsePacket();
         logoutResponsePacket.setSuccess(true);
